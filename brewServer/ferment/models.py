f5612b7ef4a8f5d4edcd73c1b116c8534df81158
Jonas Andersson <jonas@andersson.io>
2015-01-25 22:02:49 -0600

    Create Data Model

--- brewServer/ferment/models.py
+++ brewServer/ferment/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    brew_name = models.CharField(max_length=100)
+    #Store numbers up to 9999 with two decimal places
+    temperature = models.DecimalField(max_digits=6, decimal_places=2)
+    time = models.TimeField()
