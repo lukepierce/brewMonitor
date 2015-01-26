f7ca7125bf1e6a9ed052fc84ceab505b5d29e10a
Jonas Andersson <jonas@andersson.io>
2015-01-25 21:05:41 -0600

    Use JSON to represent datapoint

--- dataAcquisiton/thermoAcq.py
+++ dataAcquisiton/thermoAcq.py
@@ -5,15 +5,13 @@ from time import localtime, strftime
 brewname = "My Brown Nuts"
 f = open('workfile', 'w')
 ser = serial.Serial('/dev/ttyACM0', 9600)
-datapoints = 0
-
-
+datapoint = {'temperature': None,
+             'timestamp': None,
+             'brewname': brewname}
 
 while True:
-    datum = ser.readline()
-    f.write(strftime("%Y-%m-%d %H:%M:%S", localtime()) + ', ' + datum)
-    print(datum)
-    datapoints += 1
+    datapoint['temperature'] = ser.readline()
+    datapoint['timestamp'] = strftime("%Y-%m-%d %H:%M:%S", localtime())
 
 f.close()
 ser.close()
