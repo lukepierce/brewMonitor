d5f5a2a7aae7e989afd68631fd4c2585f6912eae
Jonas Andersson <jonas@andersson.io>
2015-01-26 00:12:04 -0600

    Change time to DateTime from Time

--- brewServer/ferment/models.py
+++ brewServer/ferment/models.py
@@ -5,4 +5,4 @@ class Data(models.Model):
     brew_name = models.CharField(max_length=100)
     #Store numbers up to 9999 with two decimal places
     temperature = models.DecimalField(max_digits=6, decimal_places=2)
-    time = models.TimeField()
+    time = models.DateTimeField()
