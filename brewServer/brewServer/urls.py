e218c179dbdc572c47142a469070f8749df6265f
root <root@raspberrypi.(none)>
2015-01-26 03:56:20 +0000

    got hello world to work

--- brewServer/brewServer/urls.py
+++ brewServer/brewServer/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'brewServer.views.home', name='home'),
+    url(r'^$', 'ferment.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
