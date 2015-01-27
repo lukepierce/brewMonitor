9b7c0a8cf734a1f3edeb8acaaa572f4e345087a8
Jonas Andersson <jonas@andersson.io>
2015-01-27 10:03:10 -0600

    Add thermocouple url

--- brewServer/brewServer/urls.py
+++ brewServer/brewServer/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'ferment.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^thermo/', 'ferment.views.thermo', name='thermo'),
     url(r'^admin/', include(admin.site.urls)),
 )
