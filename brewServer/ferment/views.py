ca894224ebe551405dd7f2acf67b32155af7a09d
Jonas Andersson <jonas@andersson.io>
2015-01-27 10:03:40 -0600

    Add thermocouple debug code to thermocouple view

--- brewServer/ferment/views.py
+++ brewServer/ferment/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+#Imports for using thermoAcq
+import serial
+import json
+from time import localtime
 # Create your views here.
 
 def home(request):
     return HttpResponse('hello world')
+
+def thermo(request):
+    brewname = "My Brown Nuts"
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    datapoint = {'temperature': None,
+                 'timestamp': None,
+                 'brewname': brewname}
+    f.close()
+    ser.close()
+    return HttpResponse(datapoint['brewname'])
