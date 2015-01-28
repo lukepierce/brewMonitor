0a702e0c5813193f9132404e5f25210fd37b1517
Jonas Andersson <jonas@andersson.io>
2015-01-27 21:51:20 -0600

    Update thermocouple url

--- brewServer/brewServer/urls.py
+++ brewServer/brewServer/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from ferment.views import thermo
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'ferment.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^thermo/', 'ferment.views.thermo', name='thermo'),
+    url(r'^thermo/', thermo.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
