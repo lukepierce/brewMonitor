059bcf38b0c485856278fa05185af6f883633451
Jonas Andersson <jonas@andersson.io>
2015-01-27 23:54:02 -0600

    Create serial connection function

--- dataAcquisiton/thermoAcq.py
+++ dataAcquisiton/thermoAcq.py
@@ -4,17 +4,23 @@ import serial
 import json
 from time import localtime
 
-brewname = "My Brown Nuts"
-ser = serial.Serial('/dev/ttyACM0', 9600)
-datapoint = {'temperature': None,
-             'timestamp': None,
-             'brewname': brewname}
+datapoint['brew_name'] = "My Brown Nuts"
+
+def serialbox(tty):
+    '''
+    Takes a 'tty' connection as input
+    Returns a serial connection object
+    Opens a serial connection to the arduino
+    '''
+    ser = serial.Serial('/dev/' + tty, 9600)
+    return ser
 
 if __name__ == '__main__'
+    conn = serialbox('ttyACM0')
     while True:
         datapoint['temperature'] = ser.readline()
         datapoint['timestamp'] = localtime()
         datapointjson = json.dumps(datapoint)
 
     f.close()
-    ser.close()
+    conn.close()
