e5395113e79dd5a6ced44f18fa1f7e892aef41ea
Jonas Andersson <jonas@andersson.io>
2015-01-27 22:50:48 -0600

    heres your message

--- brewServer/brewServer/urls.py
+++ brewServer/brewServer/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from ferment.views import thermo
 from rest_framework.urlpatterns import format_suffix_patterns
-print(thermo.__dict__)
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'ferment.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    #url(r'^thermo/', thermo.as_view()),
+    url(r'^thermo/', thermo.as_view()),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
--- brewServer/ferment/views.py
+++ brewServer/ferment/views.py
@@ -7,6 +7,8 @@ from ferment.models import Data
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
+from django.utils.six import BytesIO
 
 
 
@@ -14,9 +16,11 @@ from rest_framework.views import APIView
 def home(request):
     return HttpResponse('hello world')
 
-def thermo(APIView):
-    def put(self, format=None):
+class thermo(APIView):
+    def put(self, request,format=None):
         stream = BytesIO(request.read())
+        print(stream)
         data = JSONParser().parse(stream)
+        print(data)
         Data.objects.create(temperature=data['temperature'], brew_name=data['brew_name'])
         return Response("It fucking works")
