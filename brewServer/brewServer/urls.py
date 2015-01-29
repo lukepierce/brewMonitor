10e29898a0b29b908da4a245c1227e6bc9d209d9
Jonas Andersson <jonas123@gmail.com>
2015-01-28 21:56:07 -0600

    Minor changes

--- brewServer/brewServer/urls.py
+++ brewServer/brewServer/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from ferment.views import thermo
+from ferment.views import Thermo
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('',
