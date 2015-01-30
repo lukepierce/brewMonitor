9a9f9461ef9a4e6b3cb1ebcb1e2d781dc8bb29bc
Jonas Andersson <jonas123@gmail.com>
2015-01-29 19:07:44 -0600

    Add recipe and brew session models

--- brewServer/ferment/models.py
+++ brewServer/ferment/models.py
@@ -5,3 +5,11 @@ class Data(models.Model):
     #Store numbers up to 9999 with two decimal places
     temperature = models.DecimalField(max_digits=6, decimal_places=2)
     time = models.DateTimeField()
+
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=100)
+    recipe_style = models.CharField(max_length=100)
+
+class BrewSession(models.Model):
+    brew_date = models.DateField()
+    bottle_date = models.DateField()
