7a217dbd6dd3b56d365450621edab503d8991660
Jonas Andersson <jonas123@gmail.com>
2015-01-31 16:12:03 -0600

    Update queryset to get latest datapoint

--- brewServer/ferment/views.py
+++ brewServer/ferment/views.py
@@ -6,7 +6,6 @@ from rest_framework.renderers import JSONRenderer
 
 # Imports for using thermoAcq
 import json
-from time import localtime
 from ferment.models import Data
 # Rest Framework Imports
 from rest_framework.response import Response
@@ -20,7 +19,7 @@ def home(request):
     return HttpResponse(html)
 
 def viewData(request):
-    return render_to_response('data.html',  {'data': Data.objects.all()})
+    return render_to_response('data.html',  {'data': Data.objects.latest('time')})
 
 def viewChart(request):
     return render_to_response('chart.html',  {'data': Data.objects.all()})
@@ -34,7 +33,7 @@ class Thermo(APIView):
         print(data)
         Data.objects.create(temperature=data['temperature'], brew_name=data['brew_name'], time=data['time'])
         return Response("It fucking works")
-    
+
 class TemperatureData(ListAPIView):
     renderer_classes = (JSONRenderer, )
     serializer_class = DataSerializer
